a0ed6decfd9109acaf00657519fad5315d5b4945
SmokierLemur51 <ldl6147@gmail.com>
2025-08-10 17:55:43 -0400

    Working on product uom migration.

--- internal/db/migrations/20250810111935_products_adjustment.sql
+++ internal/db/migrations/20250810111935_products_adjustment.sql
@@ -1,16 +1,24 @@
 -- +goose Up
-create table if not exists product_units (
+create table if not exists units_of_measurements (
   id uuid primary key,
+  uom_name varchar(40) unique not null, 
+  uom_abbreviation varchar(4) 
+);
+
+create table if not exists product_uoms (
+  id uuid primary key,
+  uom_id uuid,
+  parent_uom_id uuid,
+  product_id uuid,
+  purchasing_cost real,
+  foreign key (uom_id) references units_of_measurements(id),
+  foreign key (parent_uom_id) references units_of_measurements(id),
+  foreign key (product_id) references products(id)
 );
 
 alter table products 
   add column name varchar(40);
 
-alter table products 
-  add column order_unit_id uuid;
-
-comment on column products.order_unit_id is 
-''; 
-
 -- +goose Down
-drop table if exists product_units;
+drop table if exists units_of_measurements;
+drop table if exists product_uoms;
